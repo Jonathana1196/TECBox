7ef4bbeaba97716669271c9e5fc129d927d8498e
Dario Rodriguez <darior1227@gmail.com>
2020-05-15 22:12:39 -0600

    WEBAPI FUNK

---
+++ TECBoxAPI/WebApiTECBox/WebApiTECBox/Controllers/TrabajadorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTECBox.Models;
+
+namespace WebApiTECBox.Controllers
+{
+    [Produces("application/json")]
+    [Route("Trabajadores")]
+    public class TrabajadorController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        
+        public TrabajadorController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Trabajador> GetTrabajadores()
+        {
+            return context.Trabajadores.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult PostTrabajador([FromBody]Trabajador trabajador)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Trabajadores.Add(trabajador);
+                context.SaveChanges();
+            }
+
+            return BadRequest(ModelState);
+        }
+        
+
+    }
+}
---
+++ TECBoxAPI/WebApiTECBox/WebApiTECBox/Models/Trabajador.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTECBox.Models
+{
+    public class Trabajador
+    {
+       [Key]
+        public int Cedula { get; set; }
+        
+        public string Nombre { get; set; }
+        
+        public string FechaN { get; set; }
+        
+    }
+}
