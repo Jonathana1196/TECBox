24de2fcb1da74c88ea338d95bde633bd74ce18b9
Dario Rodriguez <darior1227@gmail.com>
2020-05-21 02:43:28 -0600

    ARREGLO

--- WEB/src/app/Cliente/tecboxc/tecboxc.component.ts
+++ WEB/src/app/Cliente/tecboxc/tecboxc.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {isLineBreak} from 'codelyzer/angular/sourceMappingVisitor';
+import {timeout} from 'rxjs/operators';
+import {ProductoService} from '../../shared/ProductoService';
 
 @Component({
   selector: 'app-tecboxc',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecboxc.component.css']
 })
 export class TecboxcComponent implements OnInit {
+  Productos: FormArray = this.fb.array([]);
+  notification = null;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              // tslint:disable-next-line:no-shadowed-variable
+              private ProductoService: ProductoService) { }
 
   ngOnInit(): void {
+    this.ProductoService.getProductos().subscribe(
+      res => {
+        (res as []).forEach((Producto: any) => {
+          this.Productos.push(this.fb.group({
+            dataID : [1],
+            Codigo: [Producto.codigo, Validators.required],
+            Nombre : [Producto.nombre],
+            Descripcion : [Producto.descripcion],
+            Precio : [Producto.precio],
+            Impuesto : [Producto.impuesto],
+            Descuento : [Producto.descuento]
+          }));
+        });
+      }
+    );
   }
 
+  showNotification(category){
+    switch (category) {
+      case 'insert':
+        this.notification = {class: 'text-success', message: 'Saved'};
+        break;
+      case 'update':
+        this.notification = {class: 'text-primary', message: 'Updated'};
+        break;
+      case 'delete':
+        this.notification = {class: 'text-danger', message: 'Deleted'};
+        break;
+      default:
+        break;
+    }
+    setTimeout(() => {
+      this.notification = null;
+    }, 3000);
+  }
 }
