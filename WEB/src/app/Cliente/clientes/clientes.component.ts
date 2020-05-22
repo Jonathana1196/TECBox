138db83a8a0c7b048d852e0d2edd95ca23b6bdbb
Dario Rodriguez <darior1227@gmail.com>
2020-05-21 22:34:50 -0600

    Clientes

--- WEB/src/app/Cliente/clientes/clientes.component.ts
+++ WEB/src/app/Cliente/clientes/clientes.component.ts
@@ -1,86 +1,102 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { isLineBreak } from 'codelyzer/angular/sourceMappingVisitor';
-import { timeout } from 'rxjs/operators';
-import { ClientesService } from '../../shared/ClientesService';
-
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {isLineBreak} from 'codelyzer/angular/sourceMappingVisitor';
+import {timeout} from 'rxjs/operators';
+import {ClienteService} from '../../shared/ClientesService';
+import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
-  constructor(private fb: FormBuilder, private ClientesService: ClientesService) { }
-
-  ClientesFroms: FormArray = this.fb.array([]);
+  ClienteFroms: FormArray = this.fb.array([]);
+  UbiList = [];
   notification = null;
+  constructor(private fb: FormBuilder,
+              // tslint:disable-next-line:no-shadowed-variable
+              private ClienteService: ClienteService) { }
 
   ngOnInit(): void {
-    this.ClientesService.getClientes().subscribe(
+    this.ClienteService.getUbicacionList().subscribe(res => this.UbiList = res as []);
+    this.ClienteService.getClientes().subscribe(
       res => {
-        if (res == []) {
-          this.addClientes();
+        if (res === []) {
+          this.addCliente();
         }
-        else {
-          (res as []).forEach((Clientes: any) => {
-            this.ClientesFroms.push(this.fb.group({
-              dataID: [1],
-              Cedula: [Clientes.Cedula, Validators.required],
-              Nombre: [Clientes.Nombre],
-              Apellido: [Clientes.Apellido],
+        else{
+          (res as []).forEach((Cliente: any) => {
+            this.ClienteFroms.push(this.fb.group({
+              dataID : [1],
+              Cedula : [Cliente.cedula, Validators.required],
+              Nombre : [Cliente.nombre],
+              Apellidos: [Cliente.apellido],
+              Correo : [Cliente.correo],
+              Casillero: [Cliente.casillero],
+              TelefonoCasa : [Cliente.telefonoCasa],
+              TelefonoCelular : [Cliente.telefonoCelular],
+              Usuario : [Cliente.usuario],
+              Password : [Cliente.password],
+              Direccion: [Cliente.ubicacion],
+
             }));
           });
         }
       }
     );
   }
+  addCliente(){
+    this.ClienteFroms.push(this.fb.group({
+      dataID : [0],
+      Cedula : ['', Validators.required],
+      Nombre : [''],
+      Apellidos: [''],
+      Correo : [''],
+      Casillero: [''],
+      TelefonoCasa : [''],
+      TelefonoCelular : [''],
+      Usuario : [''],
+      Password : [''],
+      Direccion: [''],
 
-  addClientes() {
-    this.ClientesFroms.push(this.fb.group({
-      dataID: [0],
-      Cedula: ['', Validators.required],
-      Nombre: [''],
-      Apellido: [' '],
     }));
   }
-
-  recordSubmit(fg: FormGroup) {
-    if (fg.value.dataID === 0) {
+  recordSubmit(fg: FormGroup){
+    if (fg.value.dataID === 0){
       this.showNotification('insert');
-      this.ClientesService.postClientes(fg.value).subscribe(
+      this.ClienteService.postCliente(fg.value).subscribe(
         (res: any) => {
-          fg.patchValue({ Cedula: res.Cedula });
+          fg.patchValue({Cedula: res.Cedula});
         }
       );
     }
-    else {
-      this.ClientesService.putClientes(fg.value).subscribe(
+    else{
+      this.ClienteService.putCliente(fg.value).subscribe(
         (res: any) => {
           this.showNotification('update');
         }
       );
+
     }
   }
-
-  onDelete(Cedula, i) {
-    this.ClientesService.deleteClientes(Cedula).subscribe(
+  onDelete(Cedula, i){
+    this.ClienteService.deleteCliente(Cedula).subscribe(
       res => {
-        this.ClientesFroms.removeAt(i);
+        this.ClienteFroms.removeAt(i);
         this.showNotification('delete');
       }
     );
   }
 
-  showNotification(category) {
+  showNotification(category){
     switch (category) {
       case 'insert':
-        this.notification = { class: 'text-succes', message: 'Saved' };
+        this.notification = {class: 'text-success', message: 'Saved'};
         break;
       case 'update':
-        this.notification = { class: 'text-primary', message: 'Updated' };
+        this.notification = {class: 'text-primary', message: 'Updated'};
         break;
       case 'delete':
-        this.notification = { class: 'text-danger', message: 'Deleted' };
+        this.notification = {class: 'text-danger', message: 'Deleted'};
         break;
       default:
         break;
@@ -89,4 +105,5 @@ export class ClientesComponent implements OnInit {
       this.notification = null;
     }, 3000);
   }
+
 }
--- WEB/src/app/shared/ClientesService.ts
+++ WEB/src/app/shared/ClientesService.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class ClientesService {
-    constructor(private http: HttpClient) { }
-    
-    postClientes(formData) {
-        return this.http.post(environment.apiBaseURL + 'Clientes', formData);
-    }
-    putClientes(formData) {
-        return this.http.put(environment.apiBaseURL + 'Clientes' + formData.Cedula, formData);
-    }
+export class ClienteService {
 
-    deleteClientes(formData) {
-        return this.http.delete(environment.apiBaseURL + 'Clientes', formData);
-    }
-    getClientes() {
-        return this.http.get('http://127.0.0.1:5001/' + 'clientes');
+  constructor(private http: HttpClient) { }
+  getUbicacionList(){
+    return this.http.get(environment.apiBaseURL + 'Ubicaciones');
+  }
 
-    }
+  postCliente(formData) {
+    return this.http.post(environment.apiBaseURL + 'Clientes', formData);
+  }
+
+  putCliente(formData) {
+    return this.http.put(environment.apiBaseURL + 'Clientes/' + formData.Cedula, formData);
+  }
+  deleteCliente(id) {
+    return this.http.delete(environment.apiBaseURL + 'Clientes/' + id);
+  }
+
+  getClientes(){
+    return this.http.get('http://127.0.0.1:5001/' + 'Clientes');
+  }
+
+
 }
